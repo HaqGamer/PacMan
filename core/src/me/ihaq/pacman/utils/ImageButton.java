5431d149382dd0a7aabf0683e31fb72d5b1c4e75
iHaq <haqgamer66@gmail.com>
2017-06-14 20:54:52 -0400

    Version 1.8.3
    + Fixed Files

---
+++ core/src/me/ihaq/pacman/utils/ImageButton.java
@@ -0,0 +1,34 @@
+package me.ihaq.pacman.utils;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class ImageButton {
+
+	private Sprite skin;
+	private int x, y, width, height;
+
+	public ImageButton(Texture texture, int x, int y, int width, int height) {
+		this.x = x;
+		this.y = y;
+		this.width = width;
+		this.height = height;
+		skin = new Sprite(texture);
+		skin.setPosition(x, y);
+	}
+
+	public void update(SpriteBatch batch) {
+		skin.draw(batch);
+	}
+
+	public boolean isHovered(int x, int y) {
+		if (x > this.x && x < this.x + this.width) {
+			if (y > this.y && y < this.y + this.height) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
